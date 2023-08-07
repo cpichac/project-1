95cb560fc9b2296f0e438e487f177f493af5531f
dylanstoudt <138310076+dylanstoudt@users.noreply.github.com>
2023-08-07 14:17:22 -0400

    Fixed typo bug

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ console.log("Khalid")
 console.log("Dylan")
 let riseDisplay = document.querySelector("#sunriseTime")
 let setDisplay = document.querySelector("#sunsetTime")
-
-
+// Fetches and grabs sun data
 fetch("https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400")
 .then(function(response){
 
 return response.json()     
 })
+// Sets text content of display span
 .then(function (sunData) {
     let sunriseData = sunData.results.sunrise;
     let sunsetData =  sunData.results.sunset;
