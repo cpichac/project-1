8dffd61ee131550c9509b68843534afd69bc7757
Carlos Pichardo <carlos12pichardo@outlook.com>
2023-08-07 13:05:52 -0400

    created text select dropdown feature

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 console.log("Carlos")
 console.log("Khalid")
 console.log("Dylan")
+
+fetch("https://date.nager.at/api/v3/publicholidays/2023/AT")
+  .then(response => response.json())
+  .then(result => console.log(result))
