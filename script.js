769d395e1a5a32f0f135ec3d2c12aef5e36c154e
Khalid G <kgibson4795@gmail.com>
2023-08-07 14:06:01 -0400

    working on the fetch for city bikes.. created structure and trying to console log progress

--- script.js
+++ script.js
@@ -25,14 +25,29 @@ console.log("Dylan")
 
 
 
-//working
+//working api for city bikes.. (Khalid)
+//this is the 
 fetch("http://api.citybik.es/v2/networks")
-.then(function(response){
-    
-return response.json()     
-})
-.then(function (anime) {
-    console.log(anime)
+.then(response => response.json())
+.then(citiesFound => {
+    let cityFound = citiesFound[0];
+    console.log(cityFound)
+    // return fetch(`http://api.citybik.es/v2/networks`)
+
+    for (let i = 0; i < citiesFound.length; i++) {
+        let cityInput = document.querySelector("#cityInput")
+        
+    }
+
+
+
+
+
+
+// return response.json()     
+// })
+// .then(function () {
+//     console.log()
 })
 
 
