a2a28247a40a8fcbcdca8338e23bf86066ad200c
Khalid G <kgibson4795@gmail.com>
2023-08-07 10:43:10 -0400

    api ideas

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
 console.log("Carlos")
 console.log("Khalid")
 console.log("Dylan")
+
+
+// fetch("https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1")
+// .then(function(response){
+    
+// return response.json()     
+// })
+// .then(function (dogFact) {
+//     console.log(dogFact)
+// })
+
+
+//pending
+// fetch("http://api.anidb.net:9001/httpapi?request=anime")
+// .then(function(response){
+    
+// return response.json()     
+// })
+// .then(function (anime) {
+//     console.log(anime)
+// })
+
+
+
+//working
+// fetch("http://api.citybik.es/v2/networks")
+// .then(function(response){
+    
+// return response.json()     
+// })
+// .then(function (anime) {
+//     console.log(anime)
+// })
+
+
+
+// fetch("https://api.chess.com/pub/player/hikaru")
+// .then(function(response){
+    
+// return response.json()     
+// })
+// .then(function (anime) {
+//     console.log(anime)
+// })
+
