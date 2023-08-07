1f055b8d1bfb6260859e763dd5189d20d264ba20
dylanstoudt <138310076+dylanstoudt@users.noreply.github.com>
2023-08-07 13:49:18 -0400

    added js to display sunrise and sunset time

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 console.log("Carlos")
 console.log("Khalid")
 console.log("Dylan")
+let riseDisplay = document.querySelector("#sunriseTime")
+let setDisplay = document.querySelector("#sunsetTime")
+
+
+fetch("https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400")
+.then(function(response){
+
+return response.json()     
+})
+.then(function (sunData) {
+    let sunriseData = sunData.results.sunrise;
+    let sunsetData =  sunData.results.sunset;
+    riseDisplay.textContent = sunriseData;
+    setDisplay.textContent = sunsetData;
+})
