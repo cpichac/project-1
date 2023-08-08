7266450dea50af882cc37401fef3518656e7d5b2
dylanstoudt <138310076+dylanstoudt@users.noreply.github.com>
2023-08-08 13:29:59 -0400

    Page is responding correctly to input values, fixed fetch

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 let riseDisplay = document.querySelector("#sunriseTime")
 let setDisplay = document.querySelector("#sunsetTime")
+let sunButton = document.querySelector("#sunButton")
+let lat = document.querySelector("#Latitude")
+let lng = document.querySelector("#Longitude")
+
+// Function to call on click
+function displaySunData(){
 // Fetches and grabs sun data
-fetch("https://api.sunrise-sunset.org/json?lat="+lat+"&lng="+lng)
+// console.log(`lat:${lat} lng:${lng}`)
+fetch(`https://api.sunrise-sunset.org/json?lat=${lat.value}&lng=${lng.value}`)
 .then(function(response){
 
 return response.json()     
 })
 // Sets text content of display span
 .then(function (sunData) {
-    let sunriseData = sunData.results.sunrise;
-    let sunsetData =  sunData.results.sunset;
+    let sunriseData = (sunData.results.sunrise);
+    let sunsetData =  (sunData.results.sunset);
     riseDisplay.textContent = sunriseData;
     setDisplay.textContent = sunsetData;
 })
+}
+// Event listener to trigger function
+sunButton.addEventListener("click", displaySunData)
+
 
 let holidayList = document.querySelector("option").value;
 let selectHoliday = document.querySelector("#select-holiday")
 let dateEl = document.querySelector("p")
-
-
-
 
 fetch("https://date.nager.at/api/v3/publicholidays/2023/AT")
   .then(response => response.json())
